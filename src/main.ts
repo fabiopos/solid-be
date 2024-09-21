80034a3af8d9dd3af741d23a51132dfe478341ee
Fabio Posada <fabioposm@gmail.com>
2024-09-20 21:30:10 -0500

    updating vercel

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document, {});
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
